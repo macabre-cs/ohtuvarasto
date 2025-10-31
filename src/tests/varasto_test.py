465579935bf8389ff56046ffbb3137876173c7ad
Kauri-Viljar <macabre.cs@gmail.com>
2025-10-31 14:50:37 +0200

    pikkumuutos

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -22,6 +22,7 @@ class TestVarasto(unittest.TestCase):
         self.varasto.lisaa_varastoon(8)
 
         # vapaata tilaa pitäisi vielä olla tilavuus-lisättävä määrä eli 2
+        # :3
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 2)
 
     def test_ottaminen_palauttaa_oikean_maaran(self):
