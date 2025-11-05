37fe7f69357d507fd7d81a502b3fb9d5891c6e45
Kauri-Viljar <macabre.cs@gmail.com>
2025-11-05 14:04:25 +0200

    Tehtävä 6. Pylint ja koodin staattinen analyysi

--- src/index.py
+++ src/index.py
@@ -1,19 +1,30 @@
 from varasto import Varasto
 
-
 def main():
     mehua = Varasto(100.0)
     olutta = Varasto(100.0, 20.2)
 
+    tulosta_luonnin_jalkeen(mehua, olutta)
+    tulosta_getterit(olutta)
+    testaa_mehun_setterit(mehua)
+    testaa_virhetilanteet()
+    testaa_olut(olutta)
+    testaa_mehu(mehua)
+
+def tulosta_luonnin_jalkeen(mehua, olutta):
     print("Luonnin jälkeen:")
     print(f"Mehuvarasto: {mehua}")
     print(f"Olutvarasto: {olutta}")
 
+
+def tulosta_getterit(olutta):
     print("Olut getterit:")
     print(f"saldo = {olutta.saldo}")
     print(f"tilavuus = {olutta.tilavuus}")
     print(f"paljonko_mahtuu = {olutta.paljonko_mahtuu()}")
 
+
+def testaa_mehun_setterit(mehua):
     print("Mehu setterit:")
     print("Lisätään 50.7")
     mehua.lisaa_varastoon(50.7)
@@ -22,6 +33,8 @@ def main():
     mehua.ota_varastosta(3.14)
     print(f"Mehuvarasto: {mehua}")
 
+
+def testaa_virhetilanteet():
     print("Virhetilanteita:")
     print("Varasto(-100.0);")
     huono = Varasto(-100.0)
@@ -31,15 +44,12 @@ def main():
     huono = Varasto(100.0, -50.7)
     print(huono)
 
+
+def testaa_olut(olutta):
     print(f"Olutvarasto: {olutta}")
     print("olutta.lisaa_varastoon(1000.0)")
     olutta.lisaa_varastoon(1000.0)
     print(f"Olutvarasto: {olutta}")
-
-    print(f"Mehuvarasto: {mehua}")
-    print("mehua.lisaa_varastoon(-666.0)")
-    mehua.lisaa_varastoon(-666.0)
-    print(f"Mehuvarasto: {mehua}")
 
     print(f"Olutvarasto: {olutta}")
     print("olutta.ota_varastosta(1000.0)")
@@ -47,11 +57,18 @@ def main():
     print(f"saatiin {saatiin}")
     print(f"Olutvarasto: {olutta}")
 
+def testaa_mehu(mehua):
+    print(f"Mehuvarasto: {mehua}")
+    print("mehua.lisaa_varastoon(-666.0)")
+    mehua.lisaa_varastoon(-666.0)
+    print(f"Mehuvarasto: {mehua}")
+
     print(f"Mehuvarasto: {mehua}")
     print("mehua.otaVarastosta(-32.9)")
     saatiin = mehua.ota_varastosta(-32.9)
     print(f"saatiin {saatiin}")
     print(f"Mehuvarasto: {mehua}")
+
 
 
 if __name__ == "__main__":
--- src/varasto.py
+++ src/varasto.py
@@ -1,22 +1,27 @@
 class Varasto:
-    def __init__(self, tilavuus, alku_saldo = 0):
-        if tilavuus > 0.0:
-            self.tilavuus = tilavuus
-        else:
-            # virheellinen, nollataan
-            self.tilavuus = 0.0
+    def __init__(self, tilavuus, alku_saldo=0):
+        self.tilavuus = self._aseta_tilavuus(tilavuus)
+        self.saldo = self._aseta_saldo(alku_saldo)
 
+    def _aseta_tilavuus(self, tilavuus):
+        if tilavuus > 0.0:
+            return tilavuus
+        # virheellinen, nollataan
+        return 0.0
+
+    def _aseta_saldo(self, alku_saldo):
         if alku_saldo < 0.0:
             # virheellinen, nollataan
-            self.saldo = 0.0
-        elif alku_saldo <= tilavuus:
+            return 0.0
+        if alku_saldo <= self.tilavuus:
             # mahtuu
-            self.saldo = alku_saldo
-        else:
-            # täyteen ja ylimäärä hukkaan!
-            self.saldo = tilavuus
+            return alku_saldo
+        # täyteen ja ylimäärä hukkaan!
+        return self.tilavuus
 
-    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms.
+
+    # huom: ominaisuus voidaan myös laskea.
+    # Ei tarvita erillistä kenttää viela_tilaa tms.
     def paljonko_mahtuu(self):
         return self.tilavuus - self.saldo
 
